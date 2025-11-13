dfb555091cbc0c0f8b73cdf8294ed6c2b7d021cf
MikeHerro <mikey.herro@gmail.com>
2025-11-13 13:13:44 -0500

    updated header file

--- pwm.h
+++ pwm.h
@@ -1,3 +1,10 @@
+#pragma once
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
 void init_pwm_static();
 void init_pwm_irq();
 void flash_led(uint8_t team_color);
