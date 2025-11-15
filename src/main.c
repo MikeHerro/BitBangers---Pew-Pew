504564ae6dd0e1f944c0a19408cd184151648d9c
Preston Perkins <perkinsp@purdue.edu>
2025-11-15 15:01:24 -0500

    should be working for UART testing now. Constan;y prints ammo remaining. Pressing PB21 shoots it and PB26 reloads it. Will display transmited value and recieved value for each UART transmission

--- src/main.c
+++ src/main.c
@@ -16,16 +16,18 @@
 #include "hardware/uart.h"
 #include "pico/rand.h"
 
-#include "pwm.h" //our own pwm file 
+//#include "pwm.h" //our own pwm file 
 
 //////////////////////////////////////////////////////////////////////////////
-#define UART_ID      uart0
+#define UART_ID      uart1
 #define UART_BAUD    2400      //change baud rate to what we need, chat says use 2400 change it later if needed
-#define UART_TX_PIN  0
-#define UART_RX_PIN  1
+#define UART_TX_PIN  40
+#define UART_RX_PIN  41
 
-#define BUTTON_PIN   3 //active high push button 
-#define LED_HIT_PIN 10   // external LED on GPIO 10
+#define TRIGGER_PIN   21 //active high push button
+#define RELOAD_PIN   26 //active high push button
+#define LED_SHOOT_PIN 22   // external LED on GPIO 10
+#define LED_HIT_PIN 23   // external LED on GPIO 10
 
 //for sending bytes and stuff
 #define TEAM_RED    0
@@ -33,98 +35,141 @@
 #define TEAM_GREEN  2
 #define TEAM_YELLOW 3 //still dont know if we want thing (maybe make it white for all three go on?)
 
+#define MAX_ROUNDS 8
+#define SHOT_TIME 50000 // in micro seconds
+#define RELOAD_TIME 3000000 // in micro seconds
+
+uint8_t make_hit_byte();
+
 // Set the local identity of the current gun
+// Could be something configured pregame
 static uint8_t MY_PLAYER_ID = 3;        // 0 - 15 (4 bits)
 static uint8_t MY_TEAM      = TEAM_BLUE;// 0 - 3 (2 bits)
 static uint8_t MY_WEAPON    = 1;        // 0 - 3 (2 bits)
 
-//////////////////////////////////////////////////////////////////////////////
-void init_uart() {
-    // fill in
-    uart_init(UART_ID, UART_BAUD);  
+int rounds = MAX_ROUNDS;
+bool ready = false;
 
-    uart_set_hw_flow(UART_ID, false, false);
-
-    gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_TX_PIN));  //TX
-    gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_RX_PIN)); //RX
-       
-    uart_set_format(UART_ID, 8, 1, 0);
-    uart_set_fifo_enabled(UART_ID, false);
-    uart_set_irqs_enabled(UART_ID, true, false); 
-}
-
-
-void init_button(void) {
-    gpio_init(BUTTON_PIN); //use pin 3
-    gpio_set_dir(BUTTON_PIN, GPIO_IN);
-    gpio_pull_down(BUTTON_PIN); // since button is active HIGH
-}
 //////////////////////////////////////////////////////////////////////////////
 
 // packet helpers
-uint8_t make_hit_byte(uint8_t player_id_0_15, uint8_t team_0_3, uint8_t weapon_0_3)  //makes the byte for the player id set and the weapon id set
+uint8_t make_hit_byte()  //makes the byte for the player id set and the weapon id set
 {
-    return (uint8_t)(((player_id_0_15 & 0x0F) << 4) | ((team_0_3 & 0x03) << 2) | ( weapon_0_3 & 0x03));
-}
-
-void send_hit(uint8_t player_id, uint8_t team, uint8_t weapon) {
-    uint8_t b = make_hit_byte(player_id, team, weapon);
-    for (int i = 0; i < 3; i++) //the for loop gives the reciever a few chances to be hit by one press 3 bytes sent every 75 ms bassically 
-    {
-        uart_write_blocking(UART_ID, &b, 1);
-        sleep_ms(15);
-    }
+    return (uint8_t) ((MY_PLAYER_ID << 4) | (MY_TEAM << 2) | (MY_WEAPON << 0));
 }
 
 // unpack fields from one byte (for RX) 
 //old but masking, ask alex if this makes sense
-
+//will be needed for display
 uint8_t get_player(uint8_t b) { return (b >> 4) & 0x0F; } //gets the player id
 uint8_t get_team  (uint8_t b) { return (b >> 2) & 0x03; } //gets the team id
 uint8_t get_weapon(uint8_t b) { return  b       & 0x03; } //gets the weapon id
 
 //////////////////////////////////////////////////////////////////////////////
-int main() {
-    init_uart();    
-    init_button();
 
-    bool last = false;
-    
+//Handles getting glocked
+void uart_handler() {
+    uart_get_hw(UART_ID)->icr = 1u << 4;
+    uint8_t rx_data;
+    uart_read_blocking(UART_ID, &rx_data, 1);
 
-    while (1) {
-        gpio_init(15);
-        gpio_set_dir(15, 1);
-        gpio_put(15, 0);
+    printf("Recieved: %d\n", rx_data);
+}
 
-        //TX stuff
-        bool now = gpio_get(BUTTON_PIN);
-        if (now && !last) { //this will prevent someone from holding the button (edge detecting bassically)
-            send_hit(MY_PLAYER_ID, MY_TEAM, MY_WEAPON);
-            sleep_ms(20); // tiny debounce so one press = one shot
-        }
-        last = now; 
+//Initializes a single UART for TX and RX (interrupt is setup for UART1)
+void init_uart() {
+    // fill in
+    uart_init(UART_ID, UART_BAUD);
 
-        //RX stuff
-        if (uart_is_readable(UART_ID)) {
-            uint8_t b = uart_getc(UART_ID);
-            
-            uint8_t tx_team   = get_team(b);
-            uint8_t tx_player = get_player(b);
-            uint8_t tx_weapon = get_weapon(b);
-            
-            // reject friendly fire
-            if (tx_team != MY_TEAM) {
-                //we need to flash the led and pause the thing (THIS NEEDS TO BE DONE)
-                
-                gpio_put(15, 0);
-                //flash_led(tx_team); //pwm func call
-
-                sleep_ms(40); //prevents flashing the led from the same hit multiple times  
-                //gpio_put(15, 1);
-            } 
-        }
-        sleep_ms(1);
-    }
+    gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_TX_PIN)); //TX
+    gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_RX_PIN)); //RX
+       
+    uart_set_fifo_enabled(UART_ID, false);
+    uart_get_hw(UART_ID)->imsc = 1u << 4; //Enables recieve interrupt
+    irq_set_exclusive_handler(UART1_IRQ, uart_handler); //added handler
+    irq_set_enabled(UART1_IRQ, true); //sets irq enabled on nvic
 }
 
 //////////////////////////////////////////////////////////////////////////////
+
+void gpio_handler() {
+    //Check if trigger
+    if (gpio_get_irq_event_mask(TRIGGER_PIN) & GPIO_IRQ_EDGE_RISE) {
+        gpio_acknowledge_irq(TRIGGER_PIN, GPIO_IRQ_EDGE_RISE);
+
+        //Check if able to shoot
+        if (ready && rounds > 0) {
+            timer0_hw->alarm[0] = timer0_hw->timelr + SHOT_TIME;
+            uint8_t byte = make_hit_byte();
+            printf("Sent: %d\n", byte);
+            uart_write_blocking(UART_ID, &byte, 1);
+            rounds--;
+            ready = false;
+
+            //Implement PWM LEDS
+        }
+        else {
+            printf("Wrong button nigga\n");
+        }
+    }
+    //Check if reload
+    else if (gpio_get_irq_event_mask(RELOAD_PIN) & GPIO_IRQ_EDGE_RISE) {
+        gpio_acknowledge_irq(RELOAD_PIN, GPIO_IRQ_EDGE_RISE);
+
+        //Check if already full
+        if (rounds != MAX_ROUNDS) {
+            ready = false;
+            timer0_hw->alarm[0] = timer0_hw->timelr + RELOAD_TIME;
+            printf("Reloading...\n");
+            rounds = MAX_ROUNDS;
+        }
+        else {
+            printf("Wrong button nigga\n");
+        }
+    }
+}
+
+//Sets up trigger and reloads buttons as inputs and enables irq's
+void init_gpio(void) {
+    gpio_init(TRIGGER_PIN);
+    gpio_init(RELOAD_PIN);
+
+    gpio_set_dir(TRIGGER_PIN, false);
+    gpio_set_dir(RELOAD_PIN, false);
+
+    gpio_add_raw_irq_handler_masked(((1u << TRIGGER_PIN) | (1u << RELOAD_PIN)), gpio_handler);
+    gpio_set_irq_enabled(TRIGGER_PIN, GPIO_IRQ_EDGE_RISE, true);
+    gpio_set_irq_enabled(RELOAD_PIN, GPIO_IRQ_EDGE_RISE, true);
+    irq_set_enabled(21, true);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+//Allows for firing
+void shot_timer_handler() {
+    timer0_hw->intr = 1u << 0;
+    ready = true;
+}
+
+//Sets up delay between shots and after reloading
+void init_shot_timer() {
+    timer0_hw->inte = 1u << 0;
+    irq_set_exclusive_handler(0, shot_timer_handler);
+    irq_set_enabled(0, true);
+    timer0_hw->alarm[0] = timer0_hw->timelr + SHOT_TIME;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+int main() {
+    stdio_init_all();
+
+    init_uart();
+    init_gpio();
+    init_shot_timer();
+    
+    for (;;) {
+        printf("Rounds: %d\n", rounds);
+        sleep_ms(1000);
+    }
+}
